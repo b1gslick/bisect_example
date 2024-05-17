d6304ec90fcfcac1308496ed2ee07b065de1097d
b1gslick <bigslick1106@gmai.com>
2024-05-17 11:33:18 +0300

    add item

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from typing import Union
+from typing import Dict, Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+OFFER: float = 9.55
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+store: Dict[int, Item] = {}
 
 
 @app.get("/")
@@ -11,5 +23,17 @@ def read_rooot():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int):
+    return store.get(item_id)
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    if item.is_offer:
+        item.price = calculate_new_price(item.price)
+    store[item_id] = item
+    return {"item_name": item.name, "item_id": item_id}
+
+
+def calculate_new_price(price: float):
+    return price - price * OFFER
