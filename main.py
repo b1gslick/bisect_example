5532f4f1e668381124fcf19933c347c1eba613a3
b1gslick <bigslick1106@gmai.com>
2024-05-17 11:49:41 +0300

    add

--- main.py
+++ main.py
@@ -27,6 +27,14 @@ def read_item(item_id: int):
     return store.get(item_id)
 
 
+@app.post("/items/{item_id}")
+def add_item(item_id: int, item: Item):
+    if item.is_offer:
+        item.price = calculate_new_price(item.price)
+    store[item_id] = item
+    return {"item_name": item.name, "item_id": item_id}
+
+
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     if item.is_offer:
